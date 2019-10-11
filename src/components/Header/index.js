1251871c30dd5082b7d8bd3893c47e825ca5b57e
Iann Santos <iannsantos8@gmail.com>
2019-10-11 00:05:02 -0300

    Training ReactJS in Rocketseat Starter

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './styless.css'
+
+export default class Header extends Component {
+
+  render() {
+    return (
+      <header id="main-header">
+        <a href="/">JSHunt</a>
+      </header>
+    );
+  }
+}
